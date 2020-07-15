b7af5c3a0529ae7462984796883b7b4d16182c78
joao-devmaster <joaovitorsantossouza7@gmail.com>
2020-07-15 12:24:52 -0300

    Ajuste gerais finalizados

--- Controllers/NoticiasControllers.cs
+++ Controllers/NoticiasControllers.cs
@@ -3,41 +3,41 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using E_Players_AspNETCore.Models;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+using E_Players_AspNETCore.Models;
 
-
-namespace E_Players.Controllers
+namespace E_PLAYERS.Controllers
 {
-    public class NoticiaController : Controller
+    public class NoticiasController : Controller
     {
-
         Noticias noticiasModel = new Noticias();
-       
-
         public IActionResult Index()
         {
             ViewBag.Noticias = noticiasModel.ReadAll();
             return View();
         }
 
-
-           public IActionResult Cadastrar(IFormCollection form)
+        /// <summary>
+        /// Publica as informações inseridas
+        /// </summary>
+        /// <param name="form">entrada de informações</param>
+        /// <returns></returns>
+        public IActionResult Adicionar(IFormCollection form)
         {
-            Noticias novaNoticia   = new Noticias();
-            novaNoticia.IdNoticia = Int32.Parse(form["IdEquipe"]);
-            novaNoticia.Titulo     = form["Titulo"];
-            novaNoticia.Texto   = form["Texto"];
-            novaNoticia.Imagem   = form["Imagem"];
+            Noticias noticias = new Noticias();
+            noticias.IdNoticia = Int32.Parse( form["IdNoticia"]);
+            noticias.Titulo   = form["Titulo"];
+            noticias.Texto    = form["Texto"];
+            // caminho da imagem
+            noticias.Imagem   = form["Imagem"];
 
-            noticiasModel.Create(novaNoticia);            
-            ViewBag.Equipes = noticiasModel.ReadAll();
-
+            noticiasModel.Create(noticias);
+            
+            ViewBag.Noticias = noticiasModel.ReadAll();
             return LocalRedirect("~/Noticias");
         }
-
-
 
     }
 }
